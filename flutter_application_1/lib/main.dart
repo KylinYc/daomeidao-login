cd1f21ff5660bb60e88583f8a1d912494d2cdc32
KylinYc <Kylin20152004@163.com>
2024-03-30 18:54:05 +0800

    在单引号或双引号里写本次commit的注释

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,223 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // 不显示右上角的 debug
+      title: '登录界面',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      // 注册路由表
+      home: const MyHomePage(title: '登录'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  final GlobalKey _formKey = GlobalKey<FormState>();
+  late String _StudentNumber, _password;
+  bool _isObscure = true;
+  Color _eyeColor = Colors.grey;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const SizedBox(height: kToolbarHeight), // 距离顶部一个工具栏的高度
+            buildTitle(), // Login
+            buildTitleLine(), // Login下面的下划线
+            const SizedBox(height: 60),
+            buildStudentNumberTextField(), // 输入学号
+            const SizedBox(height: 30),
+            buildPasswordTextField(context), // 输入密码
+            buildForgetPasswordText(context), // 忘记密码
+            const SizedBox(height: 60),
+            buildLoginButton(context), // 登录按钮
+            const SizedBox(height: 40),
+            buildOtherLoginText(), // 其他账号登录
+            buildRegisterText(context), // 注册
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget buildRegisterText(context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('没有账号?'),
+            GestureDetector(
+              child: const Text('点击注册', style: TextStyle(color: Colors.green)),
+              onTap: () {
+                print("点击注册");
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildOtherLoginText() {
+    return const Center(
+      child: Text(
+        '其他账号登录',
+        style: TextStyle(color: Colors.grey, fontSize: 14),
+      ),
+    );
+  }
+
+  Widget buildLoginButton(BuildContext context) {
+    return Align(
+      child: SizedBox(
+        height: 45,
+        width: 270,
+        child: ElevatedButton(
+          style: ButtonStyle(
+              // 设置圆角
+              shape: MaterialStateProperty.all(const StadiumBorder(
+                  side: BorderSide(style: BorderStyle.none)))),
+          child: Text('Login',
+              style: Theme.of(context).primaryTextTheme.headline5),
+          onPressed: () {
+            // 表单校验通过才会继续执行
+            if ((_formKey.currentState as FormState).validate()) {
+              (_formKey.currentState as FormState).save();
+              //TODO 执行登录方法
+              print('Student: $_StudentNumber, password: $_password');
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget buildForgetPasswordText(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8),
+      child: Align(
+        alignment: Alignment.centerRight,
+        child: TextButton(
+          onPressed: () {
+            // Navigator.pop(context);
+            print("忘记密码");
+          },
+          child: const Text("忘记密码？",
+              style: TextStyle(fontSize: 14, color: Colors.grey)),
+        ),
+      ),
+    );
+  }
+
+  Widget buildPasswordTextField(BuildContext context) {
+    return TextFormField(
+        obscureText: _isObscure, // 是否显示文字
+        onSaved: (v) => _password = v!,
+        validator: (v) {
+          if (v!.isEmpty) {
+            return '请输入密码';
+          }
+        },
+        decoration: InputDecoration(
+            labelText: "Password",
+            suffixIcon: IconButton(
+              icon: Icon(
+                Icons.remove_red_eye,
+                color: _eyeColor,
+              ),
+              onPressed: () {
+                // 修改 state 内部变量, 且需要界面内容更新, 需要使用 setState()
+                setState(() {
+                  _isObscure = !_isObscure;
+                  _eyeColor = (_isObscure
+                      ? Colors.grey
+                      : Theme.of(context).iconTheme.color)!;
+                });
+              },
+            )));
+  }
+
+  Widget buildStudentNumberTextField() {
+    return TextFormField(
+      decoration: const InputDecoration(labelText: '学号'),
+      validator: (v) {
+        var studentNumberPattern = r'^\d{10}$'; // 假设学号为10位数字
+        RegExp studentNumberRegExp = RegExp(studentNumberPattern);
+        if (!studentNumberRegExp.hasMatch(v!)) {
+          return '请输入正确的学号';
+        }
+      },
+      onSaved: (v) => _StudentNumber = v!,
+    );
+  }
+
+  Widget buildTitleLine() {
+    return Padding(
+        padding: const EdgeInsets.only(left: 12.0, top: 4.0),
+        child: Align(
+          alignment: Alignment.bottomLeft,
+          child: Container(
+            color: Colors.black,
+            width: 40,
+            height: 2,
+          ),
+        ));
+  }
+
+  Widget buildTitle() {
+    return const Padding(
+        padding: EdgeInsets.all(8),
+        child: Text(
+          'Login',
+          style: TextStyle(fontSize: 42),
+        ));
+  }
+}
